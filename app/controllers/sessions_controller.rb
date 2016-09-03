37f7f1bd001966b64e79ac6c125c7634717ad9f7
中野 晃一朗 <nakano0531@nakano-no-MacBook-Air.local>
2016-09-04 03:34:35 +0900

    noticeの表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,16 @@ class SessionsController < ApplicationController
     password = params_user[:password]
 
     if login(email, password)
-      redirect_to root_url, notice: "successfully logged in."
+      redirect_to root_url, notice: "サインインしました"
     else
       @user = User.new(email: email)
       render :new
     end
+  end
+
+  def destroy
+    logout
+    redirect_to root_url, notice: "サインアウトしました"
   end
 
   private
