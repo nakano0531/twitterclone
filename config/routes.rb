eb0011f5221662fdd369a0b820e9ccaac0c5fcf5
中野 晃一朗 <nakano0531@nakano-no-MacBook-Air.local>
2016-09-03 22:16:40 +0900

    config/routes.rbにresources :users, only: [:index, :show]追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:index, :show]
 
   root to: 'registrations#new'
   # The priority is based upon order of creation: first created -> highest priority.
