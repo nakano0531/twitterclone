41f6994283b690152168e96ce5b0cf583c6392ef
中野 晃一朗 <nakano0531@nakano-no-MacBook-Air.local>
2016-09-03 23:28:06 +0900

    Routingの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
+  resource :settings, only: [:edit, :update]
   resources :users, only: [:index, :show]
 
   root to: 'registrations#new'
