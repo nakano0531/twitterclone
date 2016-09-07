40570169fdb87512765be04f7a0f6514af9614b3
中野 晃一朗 <nakano0531@nakano-no-MacBook-Air.local>
2016-09-08 00:51:53 +0900

    エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,21 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   resource :settings, only: [:edit, :update]
+
   resources :users, only: [:index, :show] do
+    resource :follows, only: [:create, :destroy]
     get :favorites, on: :member
+    get :follows, on: :member
+    get :followers, on: :member
   end
-  resources :tweets
+
+  resources :tweets do
     resource :favorites, only: [:create, :destroy]
+    get :timeline, on: :collection
   end
 
   root to: 'registrations#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
