cc6d66777fdf671cc91628363c5e5b30d72f8028
中野 晃一朗 <nakano0531@nakano-no-MacBook-Air.local>
2016-09-07 22:46:13 +0900

    データベースをMysqlに切り替え

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160823074845) do
+ActiveRecord::Schema.define(version: 20160907131629) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "content",    limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tweets", ["created_at"], name: "index_tweets_on_created_at", using: :btree
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",            limit: 255, null: false
