b6f899fd9f1b45ea30ac934bf10a279fbc1de02f
中野 晃一朗 <nakano0531@nakano-no-MacBook-Air.local>
2016-09-07 22:34:23 +0900

    データベースをMysqlに切り替え

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,16 @@
 ActiveRecord::Schema.define(version: 20160823074845) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",            null: false
-    t.string   "crypted_password"
-    t.string   "salt"
+    t.string   "email",            limit: 255, null: false
+    t.string   "crypted_password", limit: 255
+    t.string   "salt",             limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "screen_name"
-    t.string   "bio"
+    t.string   "name",             limit: 255
+    t.string   "screen_name",      limit: 255
+    t.string   "bio",              limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
